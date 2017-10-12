1461e46e567e143e529315bdd31f3c9df007bc61
Taylor Peat <taylorpeat@hotmail.com>
2017-10-11 20:42:06 -0400

    small problems

---
+++ ruby/sm_problems/easy_1.rb
@@ -0,0 +1,35 @@
+# Problem 1
+require 'pry'
+require 'rb-readline'
+
+def repeat(string, num)
+  num.times do
+    puts string
+  end
+end
+
+# repeat("hello", 3)
+
+# Problem 2
+
+def is_odd?(number)
+  number % 2 == 1
+end
+
+# puts is_odd?(2)    # => false
+# puts is_odd?(5)    # => true
+# puts is_odd?(-17)  # => true
+# puts is_odd?(-8)   # => false
+# puts is_odd?(0)    # => false
+# puts is_odd?(7)    # => true
+
+# Problem 3
+
+def digit_list(num)
+  num.to_s.split('').map(&:to_i)
+end
+
+puts digit_list(12345) == [1, 2, 3, 4, 5]     # => true
+puts digit_list(7) == [7]                     # => true
+puts digit_list(375290) == [3, 7, 5, 2, 9, 0] # => true
+puts digit_list(444) == [4, 4, 4]             # => true
