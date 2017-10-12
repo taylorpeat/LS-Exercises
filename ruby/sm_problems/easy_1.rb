2c5bfe334bd9f9be3d422bd7572af2403260996c
Taylor Peat <taylorpeat@hotmail.com>
2017-10-11 21:09:55 -0400

    small_problem 3

--- ruby/sm_problems/easy_1.rb
+++ ruby/sm_problems/easy_1.rb
@@ -29,7 +29,19 @@ def digit_list(num)
   num.to_s.split('').map(&:to_i)
 end
 
-puts digit_list(12345) == [1, 2, 3, 4, 5]     # => true
-puts digit_list(7) == [7]                     # => true
-puts digit_list(375290) == [3, 7, 5, 2, 9, 0] # => true
-puts digit_list(444) == [4, 4, 4]             # => true
+# puts digit_list(12345) == [1, 2, 3, 4, 5]     # => true
+# puts digit_list(7) == [7]                     # => true
+# puts digit_list(375290) == [3, 7, 5, 2, 9, 0] # => true
+# puts digit_list(444) == [4, 4, 4]             # => true
+
+# Problem 4
+
+def count_occurrences(vehicles)
+  vehicles.uniq.each do |item|
+    puts "#{item} => #{vehicles.count(item)}"
+  end
+end
+
+vehicles = ['car', 'car', 'truck', 'car', 'SUV', 'truck', 'motorcycle', 'motorcycle', 'car', 'truck']
+
+count_occurrences(vehicles)
