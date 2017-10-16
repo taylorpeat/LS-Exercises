66ee83b7cf3f408f0caf29f20b091f05faf8b283
Taylor Peat <taylorpeat@hotmail.com>
2017-10-16 18:43:06 -0400

    Problem 2

--- ruby/sm_problems/easy_2.rb
+++ ruby/sm_problems/easy_2.rb
@@ -4,4 +4,22 @@ def randomAge
   puts "Teddy is #{ rand(20..200) } years old!"
 end
 
-randomAge
+# randomAge
+
+# Problem 2
+
+SQMETERS_TO_SQFEET = 3.28084 ** 2
+
+def getRoomDimensions
+  puts "Enter the length of the room in meters:"
+  length = gets.chomp
+
+  puts "Enter the width of the room in meters:"
+  width = gets.chomp
+
+  area = length.to_f * width.to_f
+  puts "The area of the room is #{area.round(1)} square meters " +
+       "(#{(area * SQMETERS_TO_SQFEET).round(2)} square feet).\n\n"
+end
+
+getRoomDimensions
