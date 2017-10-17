c02e70134e357a785572d18851b4239025d4fee5
Taylor Peat <taylorpeat@hotmail.com>
2017-10-16 20:56:43 -0400

    Problem 5

--- ruby/sm_problems/easy_2.rb
+++ ruby/sm_problems/easy_2.rb
@@ -22,4 +22,56 @@ def getRoomDimensions
        "(#{(area * SQMETERS_TO_SQFEET).round(2)} square feet).\n\n"
 end
 
-getRoomDimensions
+# getRoomDimensions
+
+# Problem 3
+
+def tipCalculator
+  print "What is the bill? "
+  bill = gets.chomp.to_f
+
+  print "What is the tip percentage? "
+  tip_pct = gets.chomp.to_f
+
+  tip_amt = (bill * tip_pct / 100).round(2)
+  total = (bill + tip_amt).round(2)
+
+  puts "\nThe tip is $#{format("%.2f", tip_amt)}"
+  puts "The total is $#{format("%.2f", total)}\n\n"
+end
+
+# tipCalculator
+
+# Problem 4
+
+def retirementAge
+  print "What is your age? "
+  age = gets.chomp.to_i
+
+  print "At what age would you like to retire? "
+  ret_age = gets.chomp.to_i
+
+  cur_year = Time.now.year
+  years_rem = ret_age - age
+  ret_year = cur_year + years_rem
+
+  puts "\nIt's #{cur_year}. You will retire in #{ret_year}"
+  puts "You have only #{years_rem} years of work to go!\n\n"
+end
+
+# retirementAge
+
+# Problem 5
+
+def nameYell
+  print "What is your name? "
+  name = gets.chomp
+
+  if /!$/ =~ name
+    puts "HELLO #{name.chop.upcase}. WHY ARE WE SCREAMING?"
+  else
+    puts "Hello #{name}"
+  end
+end
+
+nameYell
