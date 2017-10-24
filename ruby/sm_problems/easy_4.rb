b4972e7bcad2f6a2c476404597a6b160c98c0c81
Taylor Peat <taylorpeat@hotmail.com>
2017-10-23 23:48:18 -0700

    E4P5

--- ruby/sm_problems/easy_4.rb
+++ ruby/sm_problems/easy_4.rb
@@ -93,22 +93,38 @@ def leap_year?(year)
   end
 end
 
-puts leap_year?(2016) == true
-puts leap_year?(2015) == false
-puts leap_year?(2100) == false
-puts leap_year?(2400) == true
-puts leap_year?(240000) == true
-puts leap_year?(240001) == false
-puts leap_year?(2000) == true
-puts leap_year?(1900) == false
-puts leap_year?(1752) == true
-puts leap_year?(1700) == true
-puts leap_year?(1) == false
-puts leap_year?(100) == true
-puts leap_year?(400) == true
+# puts leap_year?(2016) == true
+# puts leap_year?(2015) == false
+# puts leap_year?(2100) == false
+# puts leap_year?(2400) == true
+# puts leap_year?(240000) == true
+# puts leap_year?(240001) == false
+# puts leap_year?(2000) == true
+# puts leap_year?(1900) == false
+# puts leap_year?(1752) == true
+# puts leap_year?(1700) == true
+# puts leap_year?(1) == false
+# puts leap_year?(100) == true
+# puts leap_year?(400) == true
 
+# Problem 5
 
+def multisum(num)
+  total = 0
+  1.upto(num) do |i|
+    total += i if i % 3 == 0 || i % 5 == 0
+  end
+  total
+end
 
+def multisum_inject(num)
+  (1..num).reduce(0) { |sum, n| n % 3 == 0 || n % 5 == 0 ? sum + n : sum }
+end
+
+puts multisum(3) == 3
+puts multisum(5) == 8
+puts multisum(10) == 33
+puts multisum(1000) == 234168
 
 
 
