18b214c8e428621141d1d2718803a59895802ecf
Taylor Peat <taylorpeat@hotmail.com>
2017-11-10 13:16:02 -0500

    E6P3

--- ruby/sm_problems/easy_6.rb
+++ ruby/sm_problems/easy_6.rb
@@ -28,3 +28,38 @@ end
 
 # Problem 3
 
+def find_fibonacci_index_by_length(digits)
+  target_number = 10 ** (digits - 1)
+  recent_nums = [1,1]
+  idx = 2
+  
+  while recent_nums[1] < target_number
+    num = recent_nums.reduce(:+)
+    recent_nums.push(num).shift
+    idx += 1
+  end
+
+  idx
+end
+
+def find_fibonacci_index_by_length2(digits)
+  num = 1
+  old_num = 1
+  idx = 2
+  time = Time.now
+  
+  while num.to_s.length < digits
+    num += old_num
+    old_num = num - old_num
+    idx += 1
+  end
+
+  puts "Time elasped was " + (Time.now - time).to_s
+  idx
+end
+
+puts find_fibonacci_index_by_length(2) == 7
+puts find_fibonacci_index_by_length(10) == 45
+puts find_fibonacci_index_by_length(100) == 476
+puts find_fibonacci_index_by_length(1000) == 4782
+puts find_fibonacci_index_by_length(10000) == 47847
