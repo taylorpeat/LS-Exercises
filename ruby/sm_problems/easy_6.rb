7522c7918c3aad14d5677f305fa4d3473ffc0594
Taylor Peat <taylorpeat@hotmail.com>
2017-11-10 12:21:03 -0500

    E6P2

--- ruby/sm_problems/easy_6.rb
+++ ruby/sm_problems/easy_6.rb
@@ -1,21 +1,30 @@
 # Problem 1
 
+MIN_PER_DEG = 60
+SEC_PER_DEG = 3600
+
 def dms(num)
   deg, min_sec = num.divmod(1)
-  min, sec = min_sec.divmod(0.60)
-  sec = (sec * 100).round(0)
+  min, sec = (min_sec * SEC_PER_DEG).round(0).divmod(60)
   format(%(%d\xC2\xB0%02d'%02d\"), deg, min, sec)
 end
 
-puts dms(30)
-puts dms(30) == %(30°00'00")
-puts dms(76.73)
-puts dms(76.73) == %(76°43'48")
-puts dms(254.6)
-puts dms(254.6) == %(254°36'00")
-puts dms(93.034773)
-puts dms(93.034773) == %(93°02'05")
-puts dms(0)
-puts dms(0) == %(0°00'00")
-puts dms(360)
-puts dms(360) == %(360°00'00") || dms(360) == %(0°00'00")
+# puts dms(30) == %(30°00'00")
+# puts dms(76.73) == %(76°43'48")
+# puts dms(254.6) == %(254°36'00")
+# puts dms(93.034773) == %(93°02'05")
+# puts dms(0) == %(0°00'00")
+# puts dms(360) == %(360°00'00") || dms(360) == %(0°00'00")
+
+# Problem 2
+
+def remove_vowels(words)
+  words.map { |word| word.gsub(/[aeiou]/i, '')}
+end
+
+# puts remove_vowels(%w(abcdefghijklmnopqrstuvwxyz)) == %w(bcdfghjklmnpqrstvwxyz)
+# puts remove_vowels(%w(green YELLOW black white)) == %w(grn YLLW blck wht)
+# puts remove_vowels(%w(ABC AEIOU XYZ)) == ['BC', '', 'XYZ']
+
+# Problem 3
+
