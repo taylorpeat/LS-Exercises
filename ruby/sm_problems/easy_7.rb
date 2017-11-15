95fa99e6726dd53b404fbe9fc7baefb0f4223689
Taylor Peat <taylorpeat@hotmail.com>
2017-11-14 23:51:53 -0500

    E7P2

--- ruby/sm_problems/easy_7.rb
+++ ruby/sm_problems/easy_7.rb
@@ -8,4 +8,18 @@ def interleave(arr1, arr2)
   new_array
 end
 
-puts interleave([1, 2, 3], ['a', 'b', 'c']) == [1, 'a', 2, 'b', 3, 'c']
+# puts interleave([1, 2, 3], ['a', 'b', 'c']) == [1, 'a', 2, 'b', 3, 'c']
+
+# Problem 2
+
+def letter_case_count(string)
+  lowercase = string.gsub(/[^a-z]/, '').size
+  uppercase = string.gsub(/[^A-Z]/, '').size
+  neither = string.delete('a-zA-Z').size
+  { lowercase: lowercase, uppercase: uppercase, neither: neither }
+end
+
+puts letter_case_count('abCdef 123') == { lowercase: 5, uppercase: 1, neither: 4 }
+puts letter_case_count('AbCd +Ef') == { lowercase: 3, uppercase: 3, neither: 2 }
+puts letter_case_count('123') == { lowercase: 0, uppercase: 0, neither: 3 }
+puts letter_case_count('') == { lowercase: 0, uppercase: 0, neither: 0 }
