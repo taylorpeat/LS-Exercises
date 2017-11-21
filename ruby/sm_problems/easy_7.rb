42fb3741916fb5a2e56056a843b690a7adf42180
Taylor Peat <taylorpeat@hotmail.com>
2017-11-21 02:13:16 -0500

    E7P7

--- ruby/sm_problems/easy_7.rb
+++ ruby/sm_problems/easy_7.rb
@@ -34,6 +34,8 @@ end
 # puts word_cap('the javaScript language') == 'The Javascript Language'
 # puts word_cap('this is a "quoted" word') == 'This Is A "quoted" Word'
 
+# Problem 4
+
 def swapcase(string)
   new_string = string.chars.map do |ch|
     if ch.upcase == ch
@@ -49,6 +51,7 @@ end
 # puts swapcase('CamelCase') == 'cAMELcASE'
 # puts swapcase('Tonight on XYZ-TV') == 'tONIGHT ON xyz-tv'
 
+# Problem 5
 def staggered_case(string)
   string.downcase!
   upcase = true
@@ -57,32 +60,39 @@ def staggered_case(string)
     ch.upcase! if upcase
     upcase = !upcase
     ch
-  end.join('')
+  end.join
 end
 
-puts staggered_case('I Love Launch School!')
-puts staggered_case('ALL_CAPS') == 'AlL_CaPs'
-puts staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 NuMbErS'
+# puts staggered_case('I Love Launch School!')
+# puts staggered_case('ALL_CAPS') == 'AlL_CaPs'
+# puts staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 NuMbErS'
+
+# Problem 6
 
 def staggered_case2(string)
-  string.downcase!
-  upcase = true
+  upcase = false
 
   string.chars.map do |ch|
-    if ch.upcase != ch
-      ch.upcase! if upcase
-      upcase = !upcase
-    end
-
-    ch
-  end.join('')
+    next ch unless ch =~ /[a-z]/i
+    upcase = !upcase
+    upcase ? ch.upcase : ch.downcase
+  end.join
 end
 
 
-puts staggered_case2('I Love Launch School!') == 'I lOvE lAuNcH sChOoL!'
-puts staggered_case2('ALL CAPS') == 'AlL cApS'
-puts staggered_case2('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 nUmBeRs'
+# puts staggered_case2('I Love Launch School!') == 'I lOvE lAuNcH sChOoL!'
+# puts staggered_case2('ALL CAPS') == 'AlL cApS'
+# puts staggered_case2('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 nUmBeRs'
 
+# Problem 7
 
+def show_multiplicative_average(numbers)
+  result = numbers.reduce(1.0, :*) / numbers.length
+  puts "The result is #{sprintf('%.3f', result)}"
+end
 
+puts show_multiplicative_average([3, 5])
 
+puts show_multiplicative_average([6])
+
+puts show_multiplicative_average([2, 5, 7, 11, 13, 17])
