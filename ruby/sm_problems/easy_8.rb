0b61dd81661329ae747c28608c47135049820ef8
Taylor Peat <taylorpeat@hotmail.com>
2017-12-18 13:49:22 -0500

    E8P2

--- ruby/sm_problems/easy_8.rb
+++ ruby/sm_problems/easy_8.rb
@@ -1,10 +1,26 @@
 # Problem 1
 
 def sum_of_sums(arr)
-  arr.reverse.each.with_index.reduce(0) { |sum, (n, idx)| sum + n * (idx + 1) }
+  arr.reverse.each.with_index(1).reduce(0) { |sum, (n, idx)| sum + n * idx }
 end
 
-puts sum_of_sums([3, 5, 2]) == (3) + (3 + 5) + (3 + 5 + 2) # -> (21)
-puts sum_of_sums([1, 5, 7, 3]) == (1) + (1 + 5) + (1 + 5 + 7) + (1 + 5 + 7 + 3) # -> (36)
-puts sum_of_sums([4]) == 4
-puts sum_of_sums([1, 2, 3, 4, 5]) == 35
+# puts sum_of_sums([3, 5, 2]) == (3) + (3 + 5) + (3 + 5 + 2) # -> (21)
+# puts sum_of_sums([1, 5, 7, 3]) == (1) + (1 + 5) + (1 + 5 + 7) + (1 + 5 + 7 + 3) # -> (36)
+# puts sum_of_sums([4]) == 4
+# puts sum_of_sums([1, 2, 3, 4, 5]) == 35
+
+# Problem 2
+
+def mad_libs
+  print "Enter a noun: "
+  noun = gets.chomp
+  print "Enter a verb: "
+  verb = gets.chomp
+  print "Enter an adjective: "
+  adj = gets.chomp
+  print "Enter an adverb: "
+  adverb = gets.chomp
+  puts "\nDo you #{verb} your #{adj} #{noun} #{adverb}? That's hilarious!\n\n"
+end
+
+mad_libs
