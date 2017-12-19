9def895c97f40febd1f35de4288582b536319597
Taylor Peat <taylorpeat@hotmail.com>
2017-12-19 18:03:12 -0500

    E8P6

--- ruby/sm_problems/easy_8.rb
+++ ruby/sm_problems/easy_8.rb
@@ -31,6 +31,57 @@ def substrings_at_start(string)
   string.chars.map.with_index(1) { |_, idx| string.slice(0, idx) }
 end
 
-puts substrings_at_start('abc') == ['a', 'ab', 'abc']
-puts substrings_at_start('a') == ['a']
-puts substrings_at_start('xyzzy') == ['x', 'xy', 'xyz', 'xyzz', 'xyzzy']
+# puts substrings_at_start('abc') == ['a', 'ab', 'abc']
+# puts substrings_at_start('a') == ['a']
+# puts substrings_at_start('xyzzy') == ['x', 'xy', 'xyz', 'xyzz', 'xyzzy']
+
+# Problem 4
+
+def substrings(string)
+  string.chars.map.with_index { |_, idx| substrings_at_start(string[idx..-1]) }.flatten
+end
+
+# print substrings('abcde')
+# puts substrings('abcde') == [
+#   'a', 'ab', 'abc', 'abcd', 'abcde', 
+#   'b', 'bc', 'bcd', 'bcde',
+#   'c', 'cd', 'cde',
+#   'd', 'de',
+#   'e'
+# ]
+
+# Problem 5
+
+def palindromes(word)
+  substrings(word).select { |word| word == word.reverse && word.length > 1 }
+end
+
+# puts palindromes('abcd')
+# puts palindromes('madam') == ['madam', 'ada']
+# puts palindromes('hello-madam-did-madam-goodbye') == [
+#   'll', '-madam-', '-madam-did-madam-', 'madam', 'madam-did-madam', 'ada',
+#   'adam-did-mada', 'dam-did-mad', 'am-did-ma', 'm-did-m', '-did-', 'did',
+#   '-madam-', 'madam', 'ada', 'oo'
+# ]
+# puts palindromes('knitting cassettes') == [
+#   'nittin', 'itti', 'tt', 'ss', 'settes', 'ette', 'tt'
+# ]
+
+# Problem 6
+
+def fizzbuzz(start_num, end_num)
+  word = ''
+
+  start_num.upto(end_num) do |i|
+    word += "Fizz" if i % 3 == 0
+    word += "Buzz" if i % 5 == 0
+    word = i.to_s if word.length == 0
+    word += ', ' unless i == end_num
+    print word
+    word = ''
+  end
+  
+  puts ''
+end
+
+fizzbuzz(1, 15)
