8ec18a09ff475223955536783c9300d8e0fe90e9
Taylor Peat <taylorpeat@hotmail.com>
2017-12-30 15:49:25 -0500

    E9P6

--- ruby/sm_problems/easy_9.rb
+++ ruby/sm_problems/easy_9.rb
@@ -50,6 +50,39 @@ def sequence(num)
   arr
 end
 
-puts sequence(5) == [1, 2, 3, 4, 5]
-puts sequence(3) == [1, 2, 3]
-puts sequence(1) == [1]
+# puts sequence(5) == [1, 2, 3, 4, 5]
+# puts sequence(3) == [1, 2, 3]
+# puts sequence(1) == [1]
+
+
+# Problem 5
+
+def uppercase?(str)
+  str == str.upcase
+end
+
+# puts uppercase?('t') == false
+# puts uppercase?('T') == true
+# puts uppercase?('Four Score') == false
+# puts uppercase?('FOUR SCORE') == true
+# puts uppercase?('4SCORE!') == true
+# puts uppercase?('') == true
+
+
+# Problem 6
+
+def word_lengths(string)
+  string.split.map { |word| "#{word} #{word.length}" }
+end
+
+puts word_lengths("cow sheep chicken") == ["cow 3", "sheep 5", "chicken 7"]
+
+puts word_lengths("baseball hot dogs and apple pie") ==
+  ["baseball 8", "hot 3", "dogs 4", "and 3", "apple 5", "pie 3"]
+
+puts word_lengths("It ain't easy, is it?") == ["It 2", "ain't 5", "easy, 5", "is 2", "it? 3"]
+
+puts word_lengths("Supercalifragilisticexpialidocious") ==
+  ["Supercalifragilisticexpialidocious 34"]
+
+puts word_lengths("") == []
