dba2f33462298c27a4a039caba97dc79f99351bc
Taylor Peat <taylorpeat@hotmail.com>
2017-12-30 15:33:35 -0500

    E9P2

--- ruby/sm_problems/easy_9.rb
+++ ruby/sm_problems/easy_9.rb
@@ -4,4 +4,22 @@ def greetings(names, details)
   puts "Hello, #{names.join(' ')}! Nice to have a #{details.values.join(' ')} around."
 end
 
-greetings(['John', 'Q', 'Doe'], { title: 'Master', occupation: 'Plumber' })
+# greetings(['John', 'Q', 'Doe'], { title: 'Master', occupation: 'Plumber' })
+
+# Problem 2
+
+def twice(num)
+  left, right = num.divmod(10 ** (num.to_s.length / 2))
+  left == right ? num : num * 2
+end
+
+puts twice(37) == 74
+puts twice(44) == 44
+puts twice(334433) == 668866
+puts twice(444) == 888
+puts twice(107) == 214
+puts twice(103103) == 103103
+puts twice(3333) == 3333
+puts twice(7676) == 7676
+puts twice(123_456_789_123_456_789) == 123_456_789_123_456_789
+puts twice(5) == 10
