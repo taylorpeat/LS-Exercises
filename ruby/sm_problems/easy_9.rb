8ce2d76b82737e5a0ad43cfe662a03bd304f2669
Taylor Peat <taylorpeat@hotmail.com>
2018-01-01 23:54:07 -0500

    E9P8

--- ruby/sm_problems/easy_9.rb
+++ ruby/sm_problems/easy_9.rb
@@ -71,18 +71,43 @@ end
 
 # Problem 6
 
-def word_lengths(string)
-  string.split.map { |word| "#{word} #{word.length}" }
+# def word_lengths(string)
+#   string.split.map { |word| "#{word} #{word.length}" }
+# end
+
+# puts word_lengths("cow sheep chicken") == ["cow 3", "sheep 5", "chicken 7"]
+
+# puts word_lengths("baseball hot dogs and apple pie") ==
+#   ["baseball 8", "hot 3", "dogs 4", "and 3", "apple 5", "pie 3"]
+
+# puts word_lengths("It ain't easy, is it?") == ["It 2", "ain't 5", "easy, 5", "is 2", "it? 3"]
+
+# puts word_lengths("Supercalifragilisticexpialidocious") ==
+#   ["Supercalifragilisticexpialidocious 34"]
+
+# puts word_lengths("") == []
+
+
+# Problem 7
+
+def swap_name(name)
+  name.split.reverse.join(', ')
 end
 
-puts word_lengths("cow sheep chicken") == ["cow 3", "sheep 5", "chicken 7"]
+# puts swap_name('Joe Roberts') == 'Roberts, Joe'
 
-puts word_lengths("baseball hot dogs and apple pie") ==
-  ["baseball 8", "hot 3", "dogs 4", "and 3", "apple 5", "pie 3"]
 
-puts word_lengths("It ain't easy, is it?") == ["It 2", "ain't 5", "easy, 5", "is 2", "it? 3"]
+# Problem 8
 
-puts word_lengths("Supercalifragilisticexpialidocious") ==
-  ["Supercalifragilisticexpialidocious 34"]
+def sequence(length, step)
+  Array.new(length) { |idx| (idx + 1) * step }
+end
 
-puts word_lengths("") == []
+puts sequence(5, 1) == [1, 2, 3, 4, 5]
+puts sequence(4, -7) == [-7, -14, -21, -28]
+puts sequence(3, 0) == [0, 0, 0]
+puts sequence(0, 1000000) == []
+
+
+# Problem 9
+
