43b4b80c9fd3ec0d839eb0261a99bf033c669853
Taylor Peat <taylorpeat@hotmail.com>
2018-01-19 22:56:40 -0500

    M1P1

---
+++ ruby/sm_problems/medium_1.rb
@@ -0,0 +1,13 @@
+# Problem 1
+
+def rotate_array(arr)
+  arr[1..-1] + [arr[0]]
+end
+
+puts rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
+puts rotate_array(['a', 'b', 'c']) == ['b', 'c', 'a']
+puts rotate_array(['a']) == ['a']
+
+x = [1, 2, 3, 4]
+puts rotate_array(x) == [2, 3, 4, 1]   # => true
+x == [1, 2, 3, 4]         
