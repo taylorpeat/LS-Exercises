187e52173de6dc9c89ef3eda54d351f3725c61dd
Taylor Peat <taylorpeat@hotmail.com>
2018-01-19 23:22:26 -0500

    M1P2

--- ruby/sm_problems/medium_1.rb
+++ ruby/sm_problems/medium_1.rb
@@ -4,10 +4,24 @@ def rotate_array(arr)
   arr[1..-1] + [arr[0]]
 end
 
-puts rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
-puts rotate_array(['a', 'b', 'c']) == ['b', 'c', 'a']
-puts rotate_array(['a']) == ['a']
+# puts rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
+# puts rotate_array(['a', 'b', 'c']) == ['b', 'c', 'a']
+# puts rotate_array(['a']) == ['a']
 
-x = [1, 2, 3, 4]
-puts rotate_array(x) == [2, 3, 4, 1]   # => true
-x == [1, 2, 3, 4]         
+# x = [1, 2, 3, 4]
+# puts rotate_array(x) == [2, 3, 4, 1]   # => true
+# x == [1, 2, 3, 4]
+
+# Problem 2
+
+def rotate_rightmost_digits(num, n)
+  left, right = num.divmod(10 ** n)
+  left * 10 ** n + rotate_array(right.to_s.chars).join().to_i
+end
+
+puts rotate_rightmost_digits(735291, 1) == 735291
+puts rotate_rightmost_digits(735291, 2) == 735219
+puts rotate_rightmost_digits(735291, 3) == 735912
+puts rotate_rightmost_digits(735291, 4) == 732915
+puts rotate_rightmost_digits(735291, 5) == 752913
+puts rotate_rightmost_digits(735291, 6) == 352917
