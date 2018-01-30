cf7879f5b659861b9e9475181d8aa95ef47cf913
Taylor Peat <taylorpeat@hotmail.com>
2018-01-29 21:26:38 -0800

    M1P7

--- ruby/sm_problems/medium_1.rb
+++ ruby/sm_problems/medium_1.rb
@@ -170,3 +170,18 @@ end
 
 # minilang('6 PUSH')
 # # (nothing printed; no PRINT commands)
+
+
+# Problem 7
+DIGITS = { 
+           "one" => 1, "two" => 2, "three" => 3,
+           "four" => 4, "five" => 5, "six" => 6,
+           "seven" => 7, "eight" => 8, "nine" => 9
+         }
+
+def word_to_digit(string)
+  string.split(/\b/).map { |word| DIGITS.keys.include?(word) ? DIGITS[word] : word }.join
+end
+
+puts word_to_digit('Please call me at five five five one two three four. Thanks.')
+puts word_to_digit('Please call me at five five five one two three four. Thanks.') == 'Please call me at 5 5 5 1 2 3 4. Thanks.'
