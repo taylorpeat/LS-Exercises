4f4184e46ee727c651ab8fdd917584199aa6d56d
Taylor Peat <taylorpeat@hotmail.com>
2018-02-01 00:00:46 -0500

    M1P10

--- ruby/sm_problems/medium_1.rb
+++ ruby/sm_problems/medium_1.rb
@@ -202,11 +202,30 @@ def fibonacci(n)
   nums[1]
 end
 
-puts fibonacci(1)
-puts fibonacci(2)
-puts fibonacci(3)
-puts fibonacci(4)
-puts fibonacci(5)
-puts fibonacci(12) == 144
-puts fibonacci(20) == 6765
+# puts fibonacci(1)
+# puts fibonacci(2)
+# puts fibonacci(3)
+# puts fibonacci(4)
+# puts fibonacci(5)
+# puts fibonacci(12) == 144
+# puts fibonacci(20) == 6765
+
+# Problem 10
+
+def fibonacci_last(n)
+  first, last = [1, 1]
+  (n - 2).times do
+    first, last = [last % 10, (first + last) % 10]
+  end
+  last
+end
+
+puts fibonacci_last(15)        # -> 0  (the 15th Fibonacci number is 610)
+puts fibonacci_last(20)        # -> 5 (the 20th Fibonacci number is 6765)
+puts fibonacci_last(100)       # -> 5 (the 100th Fibonacci number is 354224848179261915075)
+puts fibonacci_last(100_001)   # -> 1 (this is a 20899 digit number)
+puts fibonacci_last(1_000_007) # -> 3 (this is a 208989 digit number)
+puts fibonacci_last(123456789) # -> 4
+
+
 
